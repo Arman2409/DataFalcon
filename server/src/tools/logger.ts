9c1a02c122079c733c1744a22a554868331598cc
Arman2409 <arman.ghazaryan@hylink.am>
2024-02-14 19:33:53 +0400

    Exception handling added

---
+++ server/src/tools/logger.ts
@@ -0,0 +1,12 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { appendFileSync } from "fs";
+
+@Injectable()
+export class CustomLogger {
+    private readonly logger = new Logger();
+
+    error(message: string) {
+        this.logger.error(message);
+        appendFileSync('./logs/errors.log', `${new Date().toISOString()} - ${message} \n`);
+    }
+}
